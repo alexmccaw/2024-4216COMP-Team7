7063fbad6b183432bb5e2a8d434702105b8dccf4
alexmccaw <alexmccaw27@gmail.com>
2024-04-13 18:34:25 +0100

    more function updates

--- dataFunctions.py
+++ dataFunctions.py
@@ -77,154 +77,151 @@ def createDualLineGraph(title, subtitle, xData, yData1, yData2, xLabel, yLabel1,
     ax1.set_ylabel(yLabel2, color = 'r')
     plt.xlabel(xLabel)
     plt.show()
-    
-def displayGraphs():
-    # create 3x3 grid of Axes in one Figure
-        fig, axs = plt.subplots(2, 3)
-        axs[0][0].set_title("Plot 1")
-        axs[0][1].set_title("Plot 2")
-        axs[0][2].set_title("Plot 3")  # Fix the index error here
-        axs[1][0].set_title("Plot 4")
-        axs[1][1].set_title("Plot 5")
-        axs[1][2].set_title("Plot 6")
-        plt.tight_layout()
-        return fig, axs
-        #plt.show()
 
-def displayAlexAnalysis():
-    xData = mapToInteger(extractColumn(14))  
-    yData = mapToInteger(extractColumn(8))
-    createLineGraph("Graph of BPM against Streams", "Example", xData, yData, "BPM", "Streams")
-    plt.show()
+def createDualLineGraphOnSubplot(ax, title, xData, yData1, yData2, xLabel, yLabel1, yLabel2):
+    ax.set_title(title)
+    ax.plot(sorted(xData), sorted(yData1), 'b', linewidth = '1')
+    ax.set_ylabel(yLabel1, color = 'b')
+    ax1=ax.twinx()
+    ax1.plot(sorted(xData), sorted(yData2), 'r', linewidth = '1')
+    ax1.set_ylabel(yLabel2, color = 'r')
+    ax.set_xlabel(xLabel)
 
-def displayLivAnalysis():
-    streamsData = mapToInteger(extractColumn(8))
-    spotifyPlaylistData = mapToInteger(extractColumn(6))
-    appleMusicPlaylistData = mapToInteger(extractColumn(9))
-    #createDualLineGraph("Streams in Spotify and Apple Playlists", "The number of streams in spotify and Apple Playlists", streamsData, spotifyPlaylistData, appleMusicPlaylistData, "Streams", "Spotify Playlist", "Apple Music Playlist")
-    fig, axs = plt.subplots(2, 3)
-    axs[0][0].set_title("Streams in Spotify and Apple Playlists")
-    axs[0][0].plot(streamsData, spotifyPlaylistData)
-    axs[0][0].plot(streamsData, appleMusicPlaylistData)
-    axs[0][1].set_title("Plot 2")
-    axs[1][0].set_title("Plot 3")
-    axs[1][1].set_title("Plot 4")
-    axs[0][2].set_title("Plot 5")
-    axs[1][2].set_title("Plot 6")
-    plt.tight_layout()
-    plt.show()
-
-def displayJakeAnalysis():
-    x
-def displayShannonAnalysis():
-    x
-def displayShaunaAnalysis():
-    x
-def displayRomaAnalysis():
-    x
-def displaySushilAnalysis():
-    x
+def createLineGraphOnSubplot(ax, title, xData, yData, xLabel, yLabel):
+    ax.set_title(title)
+    ax.plot(sorted(xData), sorted(yData), 'b', linewidth = '2')
+    ax.set_ylabel(yLabel, color = 'b')
+    ax.set_xlabel(xLabel)
 
 #Example of how to use functions
 #bpmData = mapToInteger(extractColumn(14))
 #streamsData = mapToInteger(extractColumn(8))
 #createLineGraph("Graph of BPM against Streams", "Example", bpmData, streamsData, "BPM", "Streams")
+    
+def displayAlexAnalysis():
+    #Data processing using functions
+    bpmData = mapToInteger(extractColumn(14))
+    streamsData = mapToInteger(extractColumn(8))
+    spotifyChartsData = mapToInteger(extractColumn(7))
+    appleMusicChartsData = mapToInteger(extractColumn(10))
+    
+    
+    #Plotting of figure with multiple axes, in this case, 3x1 so 3 graphs can be shown
+    fig, axs = plt.subplots(1, 2)
+
+    #Plotting individual graphs: 
+    createLineGraphOnSubplot(axs[0], "Graph of BPM against Streams", bpmData, streamsData, "BPM", "Streams")
+    createDualLineGraphOnSubplot(axs[1], "Graph of BPM against Chart Data", bpmData, spotifyChartsData,
+                                 appleMusicChartsData, "BPM", "In Spotify Charts", "In Apple Music Charts")
+    createLineGraphOnSubplot(axs[2], "Graph of BPM against Streams", bpmData, streamsData, "BPM", "Streams")
+    plt.tight_layout()
+    plt.show()
+
+def displayLivAnalysis():
+    #Data processing using functions
+    streamsData = mapToInteger(extractColumn(8))
+    spotifyPlaylistData = mapToInteger(extractColumn(6))
+    appleMusicPlaylistData = mapToInteger(extractColumn(9))
+    shazamChartsData = mapToInteger(extractColumn(13))
+    spotifyChartsData = mapToInteger(extractColumn(7))
+    appleMusicChartsData = mapToInteger(extractColumn(10))
+
+    #Plotting of figure with multiple axes, in this case, 2x3 so 6 graphs can be shown
+    fig, axs = plt.subplots(2, 3)
+
+    #Plotting individual graphs: 
+
+    createDualLineGraphOnSubplot(axs[0][0], "Streams in Spotify and Apple Playlists", streamsData, 
+                                 spotifyPlaylistData, appleMusicPlaylistData,
+                                 "Streams", "Spotify", "Apple")
+
+    createDualLineGraphOnSubplot(axs[0][1],"Streams in Spotify Playlists and Shazam Charts", streamsData, 
+                                 spotifyPlaylistData, shazamChartsData, "Streams", "Spotify Playlist", 
+                                 "Shazam Charts")
+
+    createDualLineGraphOnSubplot(axs[0][2], "Streams in Spotify Charts and Shazam Charts",  
+                                 streamsData, spotifyChartsData, shazamChartsData, "Streams", "Spotify Charts", "Shazam Charts")
+
+    createDualLineGraphOnSubplot(axs[1][0],"Streams in Apple Playlists and the Spotify Charts", streamsData,
+                          appleMusicChartsData, spotifyChartsData, "Streams", "Apple Music Charts", "Spotify Charts")
+
+    createDualLineGraphOnSubplot(axs[1][1], "Streams in the Shazam Charts and Apple Music Charts", 
+                                 streamsData, shazamChartsData, appleMusicChartsData, "Streams", "Shazam Charts", "Apple Music Charts")
+
+    createDualLineGraphOnSubplot(axs[1][2], "Streams in the Shazam Charts and the Apple Music Playlists", 
+                                 streamsData, shazamChartsData, appleMusicPlaylistData, "Streams", "Shazam Charts", "Apple Music Playlist")
+
+    plt.tight_layout()
+    plt.show()
+
+def displayJakeAnalysis():
+    # Extracting data for artist count and streams
+    artistCountData = extractColumn(2)
+    streamsData = mapToInteger(extractColumn(8))
+
+    # Creating a bar chart
+    createBarChart("Artists Count's Affect on Streams", "Example", artistCountData,
+                   streamsData, "Artist Count", "Streams(million)")
+
+def displayShannonAnalysis():
+    releaseYearData = mapToInteger(extractColumn(3))
+    releaseMonthData = mapToInteger(extractColumn(4))
+    releaseDayData = mapToInteger(extractColumn(5))
+    danceabilityData = mapToInteger(extractColumn(17))
+    valenceData = mapToInteger(extractColumn(18))
+    energyData = mapToInteger(extractColumn(19))
+
+    #Plotting of figure with multiple axes, in this case, a 3x3 figure so 9 graphs can be shown
+    fig, axs = plt.subplots(3, 3)
+
+    #Plotting individual graphs: 
+
+    createLineGraphOnSubplot(axs[0][0], "Release Year for Danceability", releaseYearData, danceabilityData, "Release Year", "Danceability")
+    createLineGraphOnSubplot(axs[1][0], "Release Month for Danceability", releaseMonthData, danceabilityData, "Release Year", "Danceability")
+    createLineGraphOnSubplot(axs[2][0], "Release Day for Danceability", releaseDayData, danceabilityData, "Release Year", "Danceability")
+    createLineGraphOnSubplot(axs[0][1], "Release Year for Valence", releaseYearData, valenceData, "Release Year", "Valence")
+    createLineGraphOnSubplot(axs[1][1], "Release Month for Valence", releaseMonthData, valenceData, "Release Year", "Valence")
+    createLineGraphOnSubplot(axs[2][1], "Release Day for Valence", releaseDayData, valenceData, "Release Year", "Valence")
+    createLineGraphOnSubplot(axs[0][2], "Release Year for Energy", releaseYearData, energyData, "Release Year", "Energy")
+    createLineGraphOnSubplot(axs[1][2], "Release Month for Energy", releaseMonthData, energyData, "Release Year", "Energy")
+    createLineGraphOnSubplot(axs[2][2], "Release Day for Energy", releaseDayData, energyData, "Release Year", "Energy")
+
+    plt.tight_layout()
+    plt.show()
+
+def displayShaunaAnalysis():
+    artistNameData = extractColumn(1)
+    spotifyPlaylistData = mapToInteger(extractColumn(6))
+    spotifyChartsData = mapToInteger(extractColumn(7))
+    applePlaylistData = mapToInteger(extractColumn(9))
+    appleChartsData = mapToInteger(extractColumn(10))
+    shazamChartsData = mapToInteger(extractColumn(13))
+
+    #Plotting of figure with multiple axes, in this case, 1x5 so 5 graphs can be shown
+    fig, axs = plt.subplots(1,5)
+
+    #Plotting individual graphs: 
+
+    createLineGraphOnSubplot(axs[0],"Artist Name In Spotify Playlists", artistNameData, spotifyPlaylistData, "Artist Name", "Spotify Playlist")
+    createLineGraphOnSubplot(axs[1], "Artist Name in Spotify Charts", artistNameData, spotifyChartsData, "Artist Name", "Spotify Charts")
+    createLineGraphOnSubplot(axs[2], "Artist Name in Apple Playlist", artistNameData, applePlaylistData, "Artist Name", "Apple Playlist")
+    createLineGraphOnSubplot(axs[3], "Artist Name in Apple Charts", artistNameData, appleChartsData, "Artist Name", "Apple Charts")
+    createLineGraphOnSubplot(axs[4], "Artist Name in Shazam Charts", artistNameData, shazamChartsData, "Artist Name", "Shazam Charts")
+    plt.tight_layout()
+    plt.show()
+
+def displayRomaAnalysis():
+    keyData = (extractColumn(15))
+    streamsData = mapToInteger(extractColumn(8))
+
+    createBarChart("Graph of Key's Effect on Streams", "How does Key affect popularity of a song?",
+                   keyData, sorted(streamsData), "Key", "Streams")
+
+def displaySushilAnalysis():
+    x
+
 
 #--- CODE YOUR INDIVIDUAL WORK IN THIS SPACE ---
-
-#artistNameData = extractColumn(1)
-#spotifyPlaylistData = mapToInteger(extractColumn(6))
-#createLineGraph("Artist Name In Spotify Playlists", "Finding the Artists in the Spotify Playlist", artistNameData, spotifyPlaylistData, "Artist Name", "Spotify Playlist")
-
-#artistNameData = extractColumn(1)
-#spotifyChartsData = mapToInteger(extractColumn(7))
-#createLineGraph("Artist Name in Spotify Charts", "Finding the Artists in the Spotify Charts", artistNameData, spotifyChartsData, "Artist Name", "Spotify Charts")
-
-#artistNameData = extractColumn(1)
-#applePlaylistData = mapToInteger(extractColumn(9))
-#createLineGraph("Artist Name in Apple Playlist", "Finding the Artists in the Apple Playlist", artistNameData, applePlaylistData, "Artist Name", "Apple Playlist")
-
-#artistNameData = extractColumn(1)
-#appleChartsData = mapToInteger(extractColumn(10))
-#createLineGraph("Artist Name in Apple Charts", "Finding the Artist in the Apple Charts", artistNameData, appleChartsData, "Artist Name", "Apple Charts")
-
-#artistNameData = extractColumn(1)
-#shazamChartsData = mapToInteger(extractColumn(13))
-#createLineGraph("Artist Name in Shazam Charts", "Finding the Artist in the Shazam Charts", artistNameData, shazamChartsData, "Artist Name", "Shazam Charts")
-
-#streamsData = mapToInteger(extractColumn(8))
-#spotifyPlaylistData = mapToInteger(extractColumn(6))
-#appleMusicPlaylistData = mapToInteger(extractColumn(9))
-#createDualLineGraph("Streams in Spotify and Apple Playlists", "The number of streams in spotify and Apple Playlists", streamsData, spotifyPlaylistData, appleMusicPlaylistData, "Streams", "Spotify Playlist", "Apple Music Playlist")
-
-#streamsData = mapToInteger(extractColumn(8))
-#spotifyPlaylistData = mapToInteger(extractColumn(6))
-#shazamChartsData = mapToInteger(extractColumn(13))
-#createDualLineGraph("Streams in Spotify Playlists and Shazam Charts", "The number of streams in Spotify Playlists and the Shazam Charts", streamsData, spotifyPlaylistData, shazamChartsData, "Streams", "Spotify Playlist", "Shazam Charts")
-
-#streamsData = mapToInteger(extractColumn(8))
-#spotifyChartsData = mapToInteger(extractColumn(7))
-#shazamChartsData = mapToInteger(extractColumn(13))
-#createDualLineGraph("Streams in Spotify Charts and Shazam Charts", "The number of streams in Spotify Charts and the Shazam Charts", streamsData, spotifyChartsData, shazamChartsData, "Streams", "Spotify Charts", "Shazam Charts")
-
-#streamsData = mapToInteger(extractColumn(8))
-#appleMusicChartsData = mapToInteger(extractColumn(10))
-#spotifyChartsData = mapToInteger(extractColumn(7))
-#createDualLineGraph("Streams in Apple Playlists and the Spotify Charts", "The number of streams in Apple Music Playlists and the Spotify Charts", streamsData, appleMusicChartsData, spotifyChartsData, "Streams", "Apple Music Charts", "Spotify Charts")
-
-#streamsData = mapToInteger(extractColumn(8))
-#shazamChartsData = mapToInteger(extractColumn(13))
-#appleMusicChartsData = mapToInteger(extractColumn(10))
-#createDualLineGraph("Streams in the Shazam Charts and Apple Music Charts", "The number of streams in Shazam Charts and the Apple Music Charts", streamsData, shazamChartsData, appleMusicChartsData, "Streams", "Shazam Charts", "Apple Music Charts")
-
-#streamsData = mapToInteger(extractColumn(8))
-#shazamChartsData = mapToInteger(extractColumn(13))
-#appleMusicPlaylistData = mapToInteger(extractColumn(9))
-#createDualLineGraph("Streams in the Shazam Charts and the Apple Music Playlists", "The number of streams in Shazam Charts and the Apple Music Playlists", streamsData, shazamChartsData, appleMusicPlaylistData, "Streams", "Shazam Charts", "Apple Music Playlist")
-    
-#keyData = (extractColumn(15))
-#streamsData = mapToInteger(extractColumn(8))
-# Zip key and stream data together, sort by keys, then unzip
-#sorted_data = sorted(zip(keyData, streamsData), key=lambda x: x[0])
-#sorted_keys, sorted_streams = zip(*sorted_data)
-#createBarChart("Graph of Key's Affect on Streams", "Example", sorted_keys, sorted_streams, "Key", "Streams")
-
-
-#releaseYearData = mapToInteger(extractColumn(3))
-#danceabilityData = mapToInteger(extractColumn(17))
-#createLineGraph("Release Year for Danceability", "Finding the trend for the release year and the danceability", releaseYearData, danceabilityData, "Release Year", "Danceability")
-
-#releaseYearData = mapToInteger(extractColumn(3))
-#valenceData = mapToInteger(extractColumn(18))
-#createLineGraph("Release Year for Valence", "Finding the trend for the release year and the valnce", releaseYearData, valenceData, "Release Year", "Valence")
-
-#releaseYearData = mapToInteger(extractColumn(3))
-#energyData = mapToInteger(extractColumn(19))
-#createLineGraph("Release Year for Energy", "Finding the trend for the release year and the Energy", releaseYearData, energyData, "Release Year", "Energy")
-
-#releaseMonthData = mapToInteger(extractColumn(4))
-#danceabilityData = mapToInteger(extractColumn(17))
-#createLineGraph("Release Month for Danceability", "Finding the trend for the release month and the Danceability", releaseMonthData, danceabilityData, "Release Month", "Danceability")
-
-#releaseMonthData = mapToInteger(extractColumn(4))
-#valenceData = mapToInteger(extractColumn(18))
-#createLineGraph("Release Month for Valence", "Finding the trend for the release month and the Valence", releaseMonthData, valenceData, "Release Month", "Valence")
-
-#releaseMonthData = mapToInteger(extractColumn(4))
-#energyData = mapToInteger(extractColumn(19))
-#createLineGraph("Release Month for Energy", "Finding the trend for the release month and the Energy", releaseMonthData, energyData, "Release Month", "Energy")
-
-#releaseDayData = mapToInteger(extractColumn(5))
-#danceabilityData = mapToInteger(extractColumn(17))
-#createLineGraph("Release Day for Danceability", "Finding the trend for the release day and the Danceability", releaseDayData, danceabilityData, "Release Day", "Danceability")
-
-#releaseDayData = mapToInteger(extractColumn(5))
-#valenceData = mapToInteger(extractColumn(18))
-#createLineGraph("Release Day for Valence", "Finding the trend for the release Day and the Valence", releaseDayData, valenceData, "Release Day", "Valence")
-
-#releaseDayData = mapToInteger(extractColumn(5))
-#energyData = mapToInteger(extractColumn(19))
-#createLineGraph("Release day for Energy", "Finding the trend for the release day and the Energy", releaseDayData, energyData, "Release Day", "Energy")
 
 # GRAPHICAL USER INTERFACE 
 # GUI menu function is below
