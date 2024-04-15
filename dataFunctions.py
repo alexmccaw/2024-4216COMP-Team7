1c4a79103ca39ec16497426977efaa2486e90c63
Shauna Hegarty <shauna.hegarty873@gmail.com>
2024-04-15 13:36:01 +0100

    shazam and apple chart graphs

--- dataFunctions.py
+++ dataFunctions.py
@@ -222,8 +222,31 @@ def displayShaunaAnalysis():
     plt.ylabel('Artist(s)')
     plt.tight_layout()
     plt.show()
-
+    # Group by artist(s)_name and count the number of entries in Apple Music charts
+    artist_apple_chart_entries = df.groupby('artist(s)_name')['in_apple_charts'].sum().sort_values(ascending=False).head(50)
+    # Create a horizontal bar plot for the top 50 artist(s) based on the number of entries in Apple Music charts
+    plt.figure(figsize=(10, 12))
+    artist_apple_chart_entries.plot(kind='barh', color='skyblue')
+    plt.title('Top 50 Artist(s) Entries in Apple Music Charts')
+    plt.xlabel('Number of Entries in Apple Music Charts')
+    plt.ylabel('Artist(s)')
+    plt.tight_layout()
+    plt.show()
     
+    # Convert 'in_shazam_charts' column to numeric, replacing non-numeric values with NaN
+    df['in_shazam_charts'] = pd.to_numeric(df['in_shazam_charts'], errors='coerce')
+    # Group by artist(s)_name and sum the entries in Shazam charts
+    artist_shazam_chart_entries = df.groupby('artist(s)_name')['in_shazam_charts'].sum().dropna().sort_values(ascending=False).head(50)
+    
+    # Create a horizontal bar plot for the top 50 artist(s) based on the number of entries in Shazam charts
+    plt.figure(figsize=(10, 12))
+    artist_shazam_chart_entries.plot(kind='barh', color='skyblue')
+    plt.title('Top 50 Artist(s) Entries in Shazam Charts')
+    plt.xlabel('Number of Entries in Shazam Charts')
+    plt.ylabel('Artist(s)')
+    plt.tight_layout()
+    plt.show()
+
 
 def displayRomaAnalysis():
     keyData = (extractColumn(15))
